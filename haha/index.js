f5378075947191786c8a2211c1d7a1d2229c8adb
philxlz06 <philipshajibaby@gmail.com>
2024-12-25 09:56:32 -0500

    Update index.js

--- haha/index.js
+++ haha/index.js
@@ -2,7 +2,7 @@ document.getElementById('searchBtn').addEventListener('click',getWeather);
 
 function getWeather(){
     const city =document.getElementById('cityInput').value;
-    const apiKey='7b2fb239db90d24971da390b21477200';
+    const apiKey='';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(url)
